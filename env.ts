f91822b9f2899f754ca84d0c23411faae2bcff0a
clai <cathyofnz@gmail.com>
2026-01-14 17:38:21 +1300

    initial draft - before adding a logout button

---
+++ env.ts
@@ -0,0 +1,10 @@
+/**
+ * Centralized env/config for values you want to commit to the repo.
+ *
+ * Note: This is a Clerk *publishable* key (safe to embed in the client).
+ */
+export const EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY =
+  "pk_test_bmF0dXJhbC1nb2xkZmlzaC0yMi5jbGVyay5hY2NvdW50cy5kZXYk" as const;
+/**
+ * EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_bmF0dXJhbC1nb2xkZmlzaC0yMi5jbGVyay5hY2NvdW50cy5kZXYk
+ */
